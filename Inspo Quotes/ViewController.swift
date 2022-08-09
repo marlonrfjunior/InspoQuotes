78a1eb5763233185248b6b64bd3689615858b8f9
Marlon Junior <marlonrfjunior@outlook.com>
2022-08-08 23:02:45 -0300

    Initial commit

---
+++ Inspo Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inspo Quotes
+//
+//  Created by Marlon Junior on 08/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
